5b3b9117b131cf32a08310b9dcbefb64c28bb9b0
perke0 <93860129+perke0@users.noreply.github.com>
2024-04-27 19:23:42 +0200

    fixing

--- c/rockPaperScissors/main.c
+++ c/rockPaperScissors/main.c
@@ -3,16 +3,54 @@
 
 
 int number(void){
-    double f = (double)rand() / RAND_MAX;
+    double f = (double)rand() / 3;
     return f * 3;
 }
+void win(void){
+    printf("You win\n");
+}
+void lose(void){
+    printf("You lose\n");
+}
+void tie(void){
+    printf("Tie\n");
+}
 int main(void){
-    char in, hand;
-    int comp = number();
+    char in;
+
     printf("Do you wanna play a game? (y/n)\n");
     scanf("%c\n", &in);
-    if(in == 'y'){
+    if(in == 'y' || in == 'Y'){
+        int comp = number();
+        int hand = 0;
         printf("Chose number:\n 1. Rock\n 2. Paper\n 3. Scissors\n");
-        scanf("%c\n", hand);
+        scanf("%d\n", &hand);
+        int ans = comp - hand;
+        switch (ans)
+        {
+        case -2:
+            lose();
+            break;
+        case -1:
+            win();
+            break;
+        case 0: 
+            tie();
+            break;
+        case 1:
+            lose();
+            break;
+        case 2:
+            win();
+            break;
+        default:
+            printf("Wrong input\n");
+            break;
+        }
+        return 0;
+    }
+    else{
+        printf("Ok :(\n");
+        return 0;
     }
 }
