ef68c04a39a5cc399e5e96b4575c2fc861a06cb4
perke0 <93860129+perke0@users.noreply.github.com>
2024-04-27 21:07:49 +0200

    Phantom input solved

--- c/rockPaperScissors/main.c
+++ c/rockPaperScissors/main.c
@@ -1,55 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <time.h>
 
 int number(void){
-    double f = (double)rand() / 3;
-    return f * 3;
+    srand(time(NULL));
+    return rand() % 3;
 }
+
 void win(void){
     printf("You win\n");
 }
+
 void lose(void){
     printf("You lose\n");
 }
+
 void tie(void){
     printf("Tie\n");
 }
-int main(void){
-    char in;
 
-    printf("Do you wanna play a game? (y/n)\n");
-    scanf("%c\n", &in);
-    if(in == 'y' || in == 'Y'){
-        int comp = number();
-        int hand = 0;
-        printf("Chose number:\n 1. Rock\n 2. Paper\n 3. Scissors\n");
-        scanf("%d\n", &hand);
-        switch (comp - hand)
-        {
-        case -2:
-            lose();
-            break;
-        case -1:
-            win();
-            break;
-        case 0: 
-            tie();
-            break;
-        case 1:
-            lose();
-            break;
-        case 2:
-            win();
-            break;
-        default:
-            printf("Wrong input\n");
-            break;
-        }
-        return 0;
-    }
-    else{
-        printf("Ok :(\n");
-        return 0;
-    }
+int main(void){
+    int hand;
+    printf("Chose number:\n 1. Rock\n 2. Paper\n 3. Scissors\n");
+    scanf(">%d\n", &hand);
+    int ans = number() - hand;
+    switch (ans)
+    {
+    case -2:
+        lose();
+        break;
+    case -1:
+        win();
+        break;
+    case 0: 
+        tie();
+        break;
+    case 1:
+        lose();
+        break;
+    case 2:
+        win();
+        break;
+    default:
+        printf("Wrong input\n");
+        break;
+    }        
+    return 0;
 }
